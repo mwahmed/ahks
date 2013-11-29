0fc8d8944a9406075bb4fbd605319bc8d2fab30f
Ubuntu <ubuntu@ip-172-31-8-129.us-west-2.compute.internal>
2013-11-29 23:25:01 +0000

    user login required to access tags/transcriptions

--- ahks/app/controllers/transcriptions_controller.rb
+++ ahks/app/controllers/transcriptions_controller.rb
@@ -1,6 +1,6 @@
 class TranscriptionsController < ApplicationController
   before_action :set_transcription, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
   # GET /transcriptions
   # GET /transcriptions.json
   def index
